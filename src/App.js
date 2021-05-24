15b39d32b01ff4c3f46390fc98faa48b177d0013
LeoCucinell <leocucinell@gmail.com>
2021-05-23 22:53:33 -0700

    comments

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
+//the app component
 function App() {
   return (
     <div className="App">
